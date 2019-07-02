2d4151ae6520959c6ac04eb1ad56a01ddfcd15ef
Syouma TERADA <sho0820ma@gmail.com>
2019-07-02 17:10:28 +0900

    first commit

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,28 @@
+class TweetsController < ApplicationController
+    #Tweetテーブルからすべてのデータを持ってくる
+    def index
+        @tweets = Tweet.all
+    end
+    
+    def show
+        @tweet = Tweet.find_by(id: params[:id])
+    end
+    
+    def new 
+        @tweet = Tweet.new
+    end
+
+    def create
+        @tweet = Tweet.new(tweet_params)
+        if @tweet.save
+          redirect_to tweets_path
+        else
+          render :new
+        end
+    end
+    
+    private
+      def tweet_params
+        params.require(:tweet).permit(:body)
+      end
+end
